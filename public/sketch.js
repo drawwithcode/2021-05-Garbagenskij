fbc57f45d9a694452c45664879f61f035087e0ae
garbagenskij <gretacozza99@gmail.com>
2021-12-12 17:32:34 +0100

    added unique color ir

--- public/sketch.js
+++ public/sketch.js
@@ -1,19 +1,49 @@
-let clientSocket = io();
-//quando mi connetto eseguo la funzione newConnection
-clientSocket.on("connect", newConnection);
-//quando invece ricevi un messaggio...
-clientSocket.on("mouseBroadcast", newBroadcast);
+//let clientSocket = io();
+let clientSocket;
+let myColor = "white";
+
+function preload() {
+  clientSocket = io();
+
+  //quando mi connetto eseguo la funzione newConnection
+  // in adavnced sketch: new Connection is called connectFunction instead
+  clientSocket.on("connect", connectFunction);
+  clientSocket.on("welcome", welcomeFunction);
+  //quando invece ricevi un messaggio...
+  // in adavnced sketch: newBrtoadcast is called mouseBroadcastFunction instead
+  clientSocket.on("mouseBroadcast", mouseBroadcastFunction);
+  clientSocket.on("newUser", newUserFunction);
+}
 
 //la funzione newConnection console logga il mio id
-function newConnection() {
-  console.log("my id is", clientSocket.id);
+// in adavnced sketch: new Connection is called connectFunction instead
+function connectFunction() {
+  console.log("your id:", clientSocket.id);
+}
+
+function welcomeFunction(data) {
+  myColor = data;
+  fill(myColor);
+  textAlign(CENTER);
+  text("Welcome " + clientSocket.id, width / 2, height / 2);
 }
 
 //quando ricevo data (il messaggio degli altri inviato a me passando per il server), console.logga data
-function newBroadcast(data) {
-  console.log(data);
-  fill("blue");
-  circle(data.x, data.y, 20);
+// in adavnced sketch: newBrtoadcast is called mouseBroadcastFunction instead
+// in adavnced sketch:their data is called dataReceived instead
+function mouseBroadcastFunction(dataReceived) {
+  console.log(dataReceived);
+  console.log("funziono");
+  fill(dataReceived.color);
+  console.log("crasho");
+  circle(dataReceived.x, dataReceived.y, 10);
+}
+
+// when a new user connects, print a welcome text
+// in adavnced sketch: newdata is called data instead
+function newUserFunction(data) {
+  fill(data.color);
+  text("New user: " + data.id, width / 2, height / 2);
 }
 
 function setup() {
@@ -22,13 +52,19 @@ function setup() {
 }
 
 function draw() {
-  fill("red");
-  circle(mouseX, mouseY, 20);
+  // draw contain my circle only in tha beginner version
+  // in advanced, it's moved to MouseMoved, so I commented them out
+  //fill("red");
+  //circle(mouseX, mouseY, 20);
 }
 
 function mouseMoved() {
+  fill(myColor);
+  circle(mouseX, mouseY, 10);
+
   //quando muovo il mouse creo automaticamente un messaggio contenente queste info al server
   let message = {
+    id: clientSocket.id,
     x: mouseX,
     y: mouseY,
   };
--- server.js
+++ server.js
@@ -15,17 +15,38 @@ let io = serverSocket(server);
 //quando accade l'evento connection: esegui la funzione newConnection
 io.on("connection", newConnection);
 
+// creaet the object that will handle users colors
+let userColors = {};
+
 //RICEVO
 // per il momento: la funzione newConnection console.logga  newsocket
 function newConnection(newSocket) {
-  console.log(newSocket.id);
+  console.log("new connection:", newSocket.id);
+
+  // generate a random hex code
+  let newColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  // add the color to the userColor object
+  // we will add a property named as the id of the client
+  // and give as value the new color
+  userColors[newSocket.id] = newColor;
+
+  // send the color code (are "to" the same as "in" as written on https://socket.io/docs/v4/rooms/??)
+  io.to(newSocket.id).emit("welcome", newColor);
+  // tell to all the others that a new user connectd
+  newSocket.broadcast.emit("newUser", { id: newSocket.id, color: newColor });
+
   //ora aggiungo il messaggio "mouse"
   //quando ricevo la mail con oggetto "mouse", eseguo la funzione Mouse Message
+  // in adavnced server.js mouseMessage is called incomingMouseMessage Instead
   newSocket.on("mouse", mouseMessage);
+
+  // callback function run when the "mouse" message is received
   //la funzione mouseMessage cosa fa? ...
   function mouseMessage(dataReceived) {
     //...prende i dati ricevuti e li console.logga...
     console.log(dataReceived);
+    // add to the data the colour
+    dataReceived.color = userColors[dataReceived.id];
     //MANDO
     //... e poi manda il messaggio con oggetto Mousebroadcast, e corpo dataReceived
     newSocket.broadcast.emit("mouseBroadcast", dataReceived);
