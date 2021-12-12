0703e2d5fa68f7a746be7bf58d05381de33c31e8
garbagenskij <gretacozza99@gmail.com>
2021-12-12 13:33:23 +0100

    added socket

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,12 @@
+let clientSocket = io();
+//quando mi connetto eseguo la funzione newConnection
+clientSocket.on("connect", newConnection);
+
+//la funzione newConnection console logga il mio id
+function newConnection() {
+  console.log(clientSocket.id);
+}
+
 function setup() {
   createCanvas(400, 400);
 }
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 console.log("up and running!!");
 
 let express = require("express");
+
 let app = express();
 let port = 3000;
 let server = app.listen(port);
 console.log("Server is running on http://localhost:" + port);
 
 app.use(express.static("public"));
+
+let serverSocket = require("socket.io");
+let io = serverSocket(server);
+
+//quando accade l'evento connection: esegui la funzione newConnection
+io.on("connection", newConnection);
+
+// per il momento: la funzione newConnection console.logga  newsocket
+function newConnection(newSocket) {
+  console.log(newSocket.id);
+}
