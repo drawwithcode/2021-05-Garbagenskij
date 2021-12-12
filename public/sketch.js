93404e6bdb658fa60f28b8128e537ce2ab4abc9e
garbagenskij <gretacozza99@gmail.com>
2021-12-12 15:02:25 +0100

    added broadcast

--- public/sketch.js
+++ public/sketch.js
@@ -1,17 +1,37 @@
 let clientSocket = io();
 //quando mi connetto eseguo la funzione newConnection
 clientSocket.on("connect", newConnection);
+//quando invece ricevi un messaggio...
+clientSocket.on("mouseBroadcast", newBroadcast);
 
 //la funzione newConnection console logga il mio id
 function newConnection() {
-  console.log(clientSocket.id);
+  console.log("my id is", clientSocket.id);
+}
+
+//quando ricevo data (il messaggio degli altri inviato a me passando per il server), console.logga data
+function newBroadcast(data) {
+  console.log(data);
+  fill("blue");
+  circle(data.x, data.y, 20);
 }
 
 function setup() {
   createCanvas(400, 400);
+  background(220);
 }
 
 function draw() {
-  background(220);
+  fill("red");
   circle(mouseX, mouseY, 20);
+}
+
+function mouseMoved() {
+  //quando muovo il mouse creo automaticamente un messaggio contenente queste info al server
+  let message = {
+    x: mouseX,
+    y: mouseY,
+  };
+  //e poi lo mando: mouse è l'oggetto della mail, message il corpo.
+  clientSocket.emit("mouse", message);
 }
--- server.js
+++ server.js
@@ -15,7 +15,19 @@ let io = serverSocket(server);
 //quando accade l'evento connection: esegui la funzione newConnection
 io.on("connection", newConnection);
 
+//RICEVO
 // per il momento: la funzione newConnection console.logga  newsocket
 function newConnection(newSocket) {
   console.log(newSocket.id);
+  //ora aggiungo il messaggio "mouse"
+  //quando ricevo la mail con oggetto "mouse", eseguo la funzione Mouse Message
+  newSocket.on("mouse", mouseMessage);
+  //la funzione mouseMessage cosa fa? ...
+  function mouseMessage(dataReceived) {
+    //...prende i dati ricevuti e li console.logga...
+    console.log(dataReceived);
+    //MANDO
+    //... e poi manda il messaggio con oggetto Mousebroadcast, e corpo dataReceived
+    newSocket.broadcast.emit("mouseBroadcast", dataReceived);
+  }
 }
