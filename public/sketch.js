4c381e02bcbf73d191566b15e0274617eeed1392
garbagenskij <gretacozza99@gmail.com>
2021-12-13 22:13:41 +0100

    Adjusted comments

--- public/sketch.js
+++ public/sketch.js
@@ -11,15 +11,15 @@ let bigratio = 1 / 3;
 let smallratio = 1 / 12;
 let puls;
 let m = 12;
-//spinning text
+//testo rotante
 let sentence =
   "you are the icing on my cake you are the icing on my cake you are the icing on my cake ";
 let sentenceArray = [];
-let r; // radius
-let deg = 1; //rotation
+let r; // raggio
+let deg = 1; //rotazione
 let theta = 0;
 
-//star text
+//testo stella
 let STAR_sentencesArray = [
   "→ You are in charge of: blue ",
   "→ You are in charge of: green ",
@@ -31,8 +31,8 @@ let STAR_sentencesArray = [
 ];
 let STAR_Sentence;
 let STAR_sentenceArray = [];
-let STAR_r = 75; // radius
-let STAR_deg = 0; //rotation
+let STAR_r = 75; // raggio
+let STAR_deg = 0; //rotazione
 let STAR_theta = 0;
 
 let myDecorations = [];
@@ -56,21 +56,20 @@ let myBgColorsArray = [
   "#FD625E",
 ];
 
-//superIndex è il valore "generato" sul server
+//superIndex sarà numero randomico tra 0 e 7 sul "generato" sul server. Ma per il momento è 0.
 let superIndex = 0;
+//superArray contiene la combo bg+image al click+ testo stella piccola per ciascuno identifier
 let SuperArray = [];
 
 function preload() {
   clientSocket = io();
 
   //quando mi connetto eseguo la funzione newConnection
-  // in adavnced sketch: new Connection is called connectFunction instead
   clientSocket.on("connect", connectFunction);
   clientSocket.on("welcome", welcomeFunction);
   //quando invece ricevi un messaggio...
-  // in adavnced sketch: newBrtoadcast is called mouseBroadcastFunction instead
   clientSocket.on("mouseBroadcast", mouseBroadcastFunction);
-  clientSocket.on("newUser", newUserFunction);
+  //clientSocket.on("newUser", newUserFunction);
 
   cake = loadImage("./assets/cake.png");
   myImageaArray[0] = loadImage("./assets/blue.png");
@@ -102,11 +101,11 @@ class newDecoration {
 }
 
 //la funzione newConnection console logga il mio id
-// in adavnced sketch: new Connection is called connectFunction instead
 function connectFunction() {
   console.log("your id:", clientSocket.id);
 }
 
+//quando ricevo il messaggio welcome, che contiene il numero randomico attribuito a *questo* client, creo il superArray
 function welcomeFunction(data) {
   superIndex = data;
   SuperArray = [
@@ -115,23 +114,17 @@ function welcomeFunction(data) {
     STAR_sentencesArray[superIndex],
   ];
   console.log(SuperArray);
-
-  //myColor = data;
-  /*fill(myColor);
-  textAlign(CENTER);
-  text("Welcome " + clientSocket.id, width / 2, height / 2);*/
 }
 
-//quando ricevo data (il messaggio degli altri inviato a me passando per il server), console.logga data
-// in adavnced sketch: newBrtoadcast is called mouseBroadcastFunction instead
-// in adavnced sketch:their data is called dataReceived instead
+//quando ricevo data (il messaggio degli altri inviato a me passando per il server)
 function mouseBroadcastFunction(dataReceived) {
-  //console.log(dataReceived);
+  //... console.logga data: riconsoci che è un immagine?
   console.log(myImageaArray[dataReceived.identifier]);
+  // yes...
 
-  //fill(dataReceived.color);
-  //console.log("crasho");
-  //circle(dataReceived.x, dataReceived.y, 10);
+  //... e pusha nel loro array le decorazioni che mi mandano gli altri
+  // utilizzando la x e la y che mi hanno comunicato loro direttamente (sotto, con la funzione mousePressed comunicherò anche io la mia)
+  // e l'identifier, che era statp attribuitogli dal server
   otherDecorations.push(
     new newDecoration(
       dataReceived.x,
@@ -144,24 +137,23 @@ function mouseBroadcastFunction(dataReceived) {
 
 // when a new user connects, print a welcome text
 // in adavnced sketch: newdata is called data instead
-function newUserFunction(data) {
-  fill(data.color);
-  text("New user: " + data.id, width / 2, height / 2);
-}
+// function newUserFunction(data) {
+//   fill(data.color);
+//   text("New user: " + data.id, width / 2, height / 2);
+// }
 
 function setup() {
   createCanvas(displayWidth, displayHeight);
 
-  //Questo non mi erve più
+  //Questo non mi serve più
   //superIndex = int(random(0, 7));
 
-  //randombg = random(myBgColorsArray);
   background(SuperArray[0]);
 
   textAlign(CENTER);
   textSize(width / 55);
   STAR_Sentence = SuperArray[2];
-  sentenceArray = sentence.split(""); // splits a string into an array of chars
+  sentenceArray = sentence.split(""); // splita la stringa in un array di caratteri
   STAR_sentenceArray = STAR_Sentence.split("");
 
   print(sentenceArray, STAR_Sentence);
@@ -169,38 +161,34 @@ function setup() {
 }
 
 function draw() {
-  // draw contain my circle only in tha beginner version
-  // in advanced, it's moved to MouseMoved, so I commented them out
-  //fill("red");
-  //circle(mouseX, mouseY, 20);
-
+  //resizo tutte le immagini
   for (u = 0; u < myImageaArray.length; u++) {
     myImageaArray[u].resize(smallratio * height, 0);
   }
+  //il bg è il primo elemento del superArray, per tutte le "versioni" del siper array.
   background(SuperArray[0]);
-  //show cake
+  //mostro la torta
   imageMode(CENTER);
-  if (width > height) {
-    cake.resize(height * ratio, 0);
-    r = height * bigratio;
-  } else {
-    cake.resize(width * ratio, 0);
-    r = width * bigratio;
-  }
+  //if (width > height) {
+  cake.resize(height * ratio, 0);
+  r = height * bigratio;
+  //} else {
+  //  cake.resize(width * ratio, 0);
+  //  r = width * bigratio;
+  // }
   image(cake, width / 2, height / 2);
 
-  //spinning text
-
+  //testo rotante
   push();
   translate(width / 2, height / 2);
   push();
 
   rotate(radians(deg));
   for (let i = 0; i < sentenceArray.length; i++) {
-    rotate((2 * PI) / sentenceArray.length); // rotation for the group of letters, which affects the spacing between letters
+    rotate((2 * PI) / sentenceArray.length); // rrotazione per il gruppo di lettere. Vi dipende lo spazio tra le lettere
     push();
     translate(r * sin(theta), r * cos(theta));
-    rotate(PI); // rotation for individual letter
+    rotate(PI); // rotazione per ciascuna lettera
     fill("white");
     noStroke();
     text(sentenceArray[i], 0, 0);
@@ -210,12 +198,13 @@ function draw() {
   deg += 0.4;
   pop();
 
+  //quelle che stanno nell'array, mostrale
   for (var i = 0; i < otherDecorations.length; i++) {
     console.log(otherDecorations[i]);
     otherDecorations[i].display();
   }
 
-  // show decoration
+  //quelle che stanno nel mioarray, mostrale
   for (var i = 0; i < myDecorations.length; i++) {
     console.log(myDecorations[i]);
     myDecorations[i].display();
@@ -233,10 +222,10 @@ function draw() {
 
   rotate(frameCount / 100);
   for (let j = 0; j < STAR_sentenceArray.length; j++) {
-    rotate((2 * PI) / STAR_sentenceArray.length); // rotation for the group of letters, which affects the spacing between letters
+    rotate((2 * PI) / STAR_sentenceArray.length);
     push();
     translate(STAR_r * sin(STAR_theta), STAR_r * cos(STAR_theta));
-    rotate(PI); // rotation for individual letter
+    rotate(PI);
     noStroke();
     textSize(30);
     fill("White");
@@ -244,19 +233,18 @@ function draw() {
     pop();
   }
   pop();
-  //STAR_deg += 0.4;
 
   pop();
 
-  //pulsing diamond
+  //diamante pulsante
   push();
 
-  // diamond
+  // diamante
   translate((width * 3) / 8, (height * 14) / 18);
   rotate(0.4 * PI);
-  //pusling diamond
+  //fallo pulsare
   scale(1 * sin(puls));
-  //static diamond
+  //scentralo
   scale(5);
 
   //fill
@@ -342,10 +330,7 @@ function mousePressed() {
   if (dist(mouseX, mouseY, width / 2, height / 2) <= 0.8 * r) {
     myDecorations.push(new newDecoration(mouseX, mouseY, SuperArray[1]));
 
-    /*fill(myColor);
-  circle(mouseX, mouseY, 10);*/
-
-    //quando muovo il mouse creo automaticamente un messaggio contenente queste info al server
+    //quando muovo il mouse creo automaticamente un messaggio contenente queste info
     let message = {
       id: clientSocket.id,
       x: mouseX,
@@ -362,10 +347,10 @@ function star(x, y, radius1, radius2, npoints) {
   let angle = TWO_PI / npoints;
   let halfAngle = angle / 2.0;
   beginShape();
-  //uncomment for matching filling
+  // match il fill
   fill(SuperArray[0]);
 
-  // uncomment for sparaflashing
+  // uncomment per sparaflashing
   // fill(random(myBgColorsArray));
 
   stroke("white");
--- server.js
+++ server.js
@@ -15,56 +15,36 @@ let io = serverSocket(server);
 //quando accade l'evento connection: esegui la funzione newConnection
 io.on("connection", newConnection);
 
-// creaet the object that will handle users colors
-//let userColors = {};
+// creo l'oggetto che accoppierà alla key "id" il value "random(0,7"
 let userIdentifiers = {};
 
-//RICEV
-// per il momento: la funzione newConnection console.logga  newsocket
+//  la funzione newConnection console.logga  newsocket
 function newConnection(newSocket) {
   //console.log(newSocket);
   console.log("new connection:", newSocket.id);
 
-  // generate a random hex code
-  //let newColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  //genero il numero random tra 0 e 7 (a ogni numero corrisponde una "versione" dello sketch)
   var newIdentifier = Math.floor(Math.random() * 7);
   console.log(newIdentifier);
-  // add the color to the userColor object
-  // we will add a property named as the id of the client
-  // and give as value the new color
-  //userColors[newSocket.id] = newColor;
+  //aggiungo all'oggetto userIdentifier una key che ha lo stesso nome dell'id, a cui attribuisco il value random tra 0 e 7 generato con newIdentifier
   userIdentifiers[newSocket.id] = newIdentifier;
 
-  // send the color code (are "to" the same as "in" as written on https://socket.io/docs/v4/rooms/??)
-  io.to(newSocket.id).emit(
-    "welcome",
-    //newColor
-    newIdentifier
-  );
-
-  //-------------FORTI---DUBBI---------------------
-  // tell to all the others that a new user connectd
-  newSocket.broadcast.emit("newUser", {
-    id: newSocket.id,
-    //color: newColor,
-    identifier: newIdentifier,
-  });
+  // mando a *quel* client il suo numero random tra 0 e 7( "to" e "in" sono lo stesso secondo https://socket.io/docs/v4/rooms/??)
+  io.to(newSocket.id).emit("welcome", newIdentifier);
 
   //ora aggiungo il messaggio "mouse"
   //quando ricevo la mail con oggetto "mouse", eseguo la funzione Mouse Message
-  // in adavnced server.js mouseMessage is called incomingMouseMessage Instead
   newSocket.on("mouse", mouseMessage);
 
-  // callback function run when the "mouse" message is received
+  // la callback function viene eseguita quando ricevo "mouse"
   //la funzione mouseMessage cosa fa? ...
   function mouseMessage(dataReceived) {
     //...prende i dati ricevuti e li console.logga...
-    console.log("Biba", dataReceived);
-    // add to the data the colour
-    //dataReceived.color = userColors[dataReceived.id];
+    console.log("dati ricevuti", dataReceived);
+    // ... aggiunge il valore randomico tra 0 e 7 ai dati ricevuti
     dataReceived.identifier = userIdentifiers[dataReceived.id];
-    console.log("Greta", dataReceived);
-    //MANDO
+    //... li ri-console-logga
+    console.log("dati aggiornati", dataReceived);
     //... e poi manda il messaggio con oggetto Mousebroadcast, e corpo dataReceived
     newSocket.broadcast.emit("mouseBroadcast", dataReceived);
   }
